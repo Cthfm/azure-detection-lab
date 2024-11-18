363514da65b307e0cb5d53de0e70a5be97b23d4f
CTHFM <cthfm.sec@gmail.com>
2024-11-18 15:20:07 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "detection-lab-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "Detection Lab"
+}
+
+variable "admin_username" {
+  description = "Admin username for VMs"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for Windows VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
